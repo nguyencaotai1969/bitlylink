b95e1ff73fc0d383ff690e257a69bb83df1a0222
nguyencaotai1969 <nguyencaotai1969>
2021-10-18 17:15:37 +0700

    s

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       </ul>
-      <button data-toggle="modal" data-target="#exampleModal" class="btn btn-outline-success my-2 my-sm-0" type="submit">Kiếm tiềnoke nguyễn cao tai</button>
+      <button data-toggle="modal" data-target="#exampleModal" class="btn btn-outline-success my-2 my-sm-0" type="submit">Kiếm tiềnoke nguyễn cao taisss</button>
    </div>
 </nav>
 <div class="container" id="app">
